c50dac14523dcd2b2022b61494b00a3949933e89
egig <egigundari@gmail.com>
2015-01-07 15:13:56 +0700

    demo completion

--- demo/index.php
+++ demo/index.php
@@ -1,1 +1,34 @@
-<?php //..
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Silex\Application(['debug' => true]);
+
+is_dir(__DIR__.'/files') or
+	die('You need to create "files" directory under "demo" directory contains files yout want to browse');
+
+$app['finder'] = new Drafterbit\Finder(__DIR__.'/files');
+
+$app->get('/', function () use ($app) {
+	return file_get_contents('template.php');
+});
+
+$app->get('/data', function () use ($app) {
+
+	$data = array();
+	$path = $app['request']->get('path');
+	switch ($app['request']->get('op')) {
+		
+		case 'ls':
+			$data = $app['finder']->ls($path);
+			break;
+		
+		default:
+			# code...
+			break;
+	}
+
+	return $app->json($data);
+});
+
+$app->run();
