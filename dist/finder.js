9a35bdecf98586153352b4bca0debfb15fa3c6c7
egig <egigundari@gmail.com>
2015-06-17 16:45:52 +0700

    fix default locale

--- dist/finder.js
+++ dist/finder.js
@@ -503,9 +503,19 @@ DTFINDER.DOM = {
 	locale: null,
 
 	localize: function(string) {
-		if(typeof DTFINDER.lang[this.locale][string] == 'undefined' ) {
+
+		if(typeof DTFINDER.lang[this.locale] == 'undefined' ) {
 			return string;
 		} else {
+			if(typeof DTFINDER.lang[this.locale][string] == 'undefined' ) {
+
+				if(this.local !== 'en') {
+					console.warn("Missing translation: '"+string+"' for locale "+ this.locale);
+				}
+
+				return string;
+			}
+			
 			return DTFINDER.lang[this.locale][string];
 		}
 	}
