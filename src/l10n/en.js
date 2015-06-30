f36b7f1c5a39e57066e53d00ae31461d7e67d92c
egig <egigundari@gmail.com>
2015-06-30 15:54:30 +0700

    add en languagee file

---
+++ src/l10n/en.js
@@ -0,0 +1,18 @@
+DTFINDER.lang['en'] = {
+	'Upload': 'Upload',
+	'Download': 'Download',
+	'New Folder': 'New Folder',
+	'Move': 'Move',
+	'Delete': 'Delete',
+	'Properties': 'Properties',
+	'Rename': 'Rename',
+	'Search': 'Search',
+	'Submit': 'Submit',
+	'Select': 'Select',
+	'Cancel': 'Cancel',
+	'Name': 'Name',
+	'Type': 'Type',
+	'Size': 'Size',
+	'Location' : 'Location',
+	'Folder Name': 'Folder Name'
+}
