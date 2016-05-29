cf18404bbe0e4e960a140a0744d84a09214b5cc0
egig <egigundari@gmail.com>
2016-05-29 17:15:44 +0700

    add upload feature

--- server.js
+++ server.js
@@ -4,11 +4,45 @@ var express = require('express')
 var path = require('path')
 var fs = require('fs')
 var app = express()
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
+var rimraf = require('rimraf')
+var multer = require('multer')
+var multer  = require('multer')
+var upload = multer({ dest: 'files/' })
 
 var base_path = path.join(__dirname, 'files');
 
 var Server = {
+
+    handleUpload: function(request, response) {
+
+        var q_path = request.body.path;
+        var reqx_path = prepare_path(q_path);
+
+        var json_response = [];
+        for(var i=0; i<request.files.length; i++) {
+
+            var tmp_path = request.files[i].path;
+
+            // The original name of the uploaded file
+            // is stored in the variable "originalname".
+            var name = request.files[i].originalname;
+            var target_path = reqx_path+'/' + request.files[i].originalname;
+
+            var src = fs.createReadStream(tmp_path);
+            var dest = fs.createWriteStream(target_path);
+            src.pipe(dest);
+
+            json_response.push({uploaded: name});
+
+            // @todo
+            // src.on('end', function() { res.render('complete'); });
+            /// src.on('error', function(err) { res.render('error'); });
+        }
+
+        response.json(json_response);
+    },
+
     handle: function(request, response) {
 
         var op = request.query.op;
@@ -31,12 +65,24 @@ var Server = {
     
                 var json_response = this.mkdir(q_path, folder_name);
                 break;
+            case 'delete':
+                q_path = request.body.path;
+                var json_response = this.delete(q_path);
+                break;
             default:
                 var json_response = {error: "Unknown op"};
                 break;
         }
 
         response.json(json_response);
+    },
+
+    delete: function(q_path) {
+        var reqx_path = prepare_path(q_path);
+
+        rimraf(reqx_path, function(err) { });
+
+        return {};
     },
 
     mkdir: function(q_path, folder_name) {
@@ -117,6 +163,13 @@ app.get("/files", function(request, response) {
 app.post("/files", function(request, response) {
     Server.handle(request, response);
 });
+
+var uploadType = upload.array('files[]', 10);
+
+app.post('/files/upload', uploadType, function (req, res, next) {
+    Server.handleUpload(req, res);
+});
+
 
 app.listen(port);
 console.log("Running server on port "+port);
